c734aa0a668e650a96260b208e4ad1dd80abbae9
Stephen Wan <stephen@stephenwan.net>
2014-02-06 03:40:59 -0600

    refactored rtsp response object

--- test/server.js
+++ test/server.js
@@ -1,8 +1,14 @@
-var nodetunes = require('../index.js');
+var AirTunesServer = require('../index');
 var Speaker = require('speaker');
 
-nodetunes(new Speaker({
+var server = new AirTunesServer(new Speaker({
   channels: 2,
   bitDepth: 16,
   sampleRate: 44100
 }));
+
+server.start();
+
+server.on('volumeChange', function(vol) {
+	console.log(vol);
+});
