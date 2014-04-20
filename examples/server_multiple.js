1438f65db92e27b995550a388e98da1d942d63bd
Stephen Wan <stephen@stephenwan.net>
2014-04-20 04:45:28 -0500

    naively fixed some scoping issues. covers at least fixing scoping-leaking when using multiple nodetunes instances at once

---
+++ examples/server_multiple.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var AirTunesServer = require('../index');
+var Speaker = require('speaker');
+
+var server1 = new AirTunesServer({ serverName: 'NodeTunes 1' });
+var server2 = new AirTunesServer({ serverName: 'NodeTunes 2' });
+
+server1.on('clientConnected', function(stream) {
+	stream.on('data', function(d) {
+		process.stdout.write('\rWriting for Server 1: ' + d.length + ' bytes @ ' + new Date().getTime() + '\t');	
+	})
+});
+
+server2.on('clientConnected', function(stream) {	
+	stream.on('data', function(d) {
+		process.stdout.write('\rWriting for Server 2: ' + d.length + ' bytes @ ' + new Date().getTime() + '\t');	
+	})
+});
+
+server1.start();
+server2.start();
